178df1a76166f5bc44f1d4c008d7632116a8501e
sharkdon <72847492+Mount-Isaac@users.noreply.github.com>
2022-09-08 11:15:43 +0300

    Create my_utils.py

---
+++ my_utils.py
@@ -0,0 +1,11 @@
+#decorator function which is used to measure the Tn of each algorithm
+import time
+
+def time_it(func):
+  def wrapper(*args, **kwargs):
+    start = time.time()
+    result = func(*args, **kwargs)
+    end = time.time()
+    print(f'{fun.__name__} took {str((end-start) * 1000)} milliseconds')
+    return result
+  return wrapper
