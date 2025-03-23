0889c07bce7aeeb6f5591a146864a889b97bf63b
isaac kyalo <kyalomutie@students.ku.ac.ke>
2025-03-23 23:45:55 +0300

    added insert, print & reverse a single linkedlist

--- golang/datastructures/singly_linkedlist.go
+++ golang/datastructures/singly_linkedlist.go
@@ -1,20 +1,67 @@
 package datastructures
 
+import (
+	"fmt"
+)
+
 type Node struct {
 	val  int
 	next *Node
 }
 
-func (list Node) InsertSinglyLinkedList(data []int) Node {
-	// insert head
-	if list.next.val == nil {
-		return list
+type LinkedList struct {
+	head *Node
+}
+
+func (list *LinkedList) InsertSinglyLinkedList(data int) {
+	// create a new node
+	newNode := &Node{val: data}
+
+	if list.head == nil {
+		list.head = newNode
 	} else {
-		list = Node{
-			val:  data[0],
-			next: &Node{},
+		current := list.head
+		for current.next != nil {
+			current = current.next
 		}
+		current.next = newNode
+	}
+}
+
+func (list *LinkedList) PrintNodes(*LinkedList) {
+	current := list.head
+	if current == nil {
+		fmt.Println("Linked list is empty")
+		return
 	}
 
-	return list
+	fmt.Println("Linked list: ")
+	for current != nil {
+		if current.next == nil {
+			//  don't print --> on tail node
+			fmt.Printf("%d", current.val)
+		} else {
+			fmt.Printf("%d-->", current.val)
+		}
+		current = current.next
+	}
+	fmt.Println()
+}
+
+func (list *LinkedList) ReverseSinglyLList(*LinkedList) LinkedList {
+	current := list.head
+	var prev *Node
+
+	if current == nil {
+		return *list
+	}
+
+	for current != nil {
+		nextNode := current.next
+		current.next = prev
+		prev = current
+		current = nextNode
+	}
+	list.head = prev
+	return *list
 }
--- golang/main.go
+++ golang/main.go
@@ -3,6 +3,17 @@ package main
 import "dsa/datastructures"
 
 func main() {
-	node_object := datastructures.Node{}
-	node_object.InsertSinglyLinkedList([]int{1, 2, 3, 5, 6, 7})
+	// instantiate an object of a singly linked list (sll)
+	sll_obj := datastructures.LinkedList{}
+
+	//  insert nodes from a slice of elements
+	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	for _, number := range nums {
+		sll_obj.InsertSinglyLinkedList(number)
+	}
+
+	// print the inserted nodes
+	sll_obj.PrintNodes(&sll_obj)
+	sll_obj.ReverseSinglyLList(&sll_obj)
+	sll_obj.PrintNodes(&sll_obj)
 }
